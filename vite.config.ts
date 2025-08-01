151125726a8ab02609f7de29f4829b56449cfeee
Aziz Rozmetov <argringo97@gmail.com>
2025-08-02 02:39:28 +0500

    Updated manifest

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,30 @@ export default defineConfig({
         display: 'standalone',
         background_color: '#ffffff',
         theme_color: '#4caf50',
-        icons: [ /* ... */]
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-maskable-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/pwa-maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ]
       },
       workbox: {
         navigateFallback: '/offline.html',
