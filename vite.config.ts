865b75fc990a25effc5b610a1f9f23c074194c78
Aziz Rozmetov <argringo97@gmail.com>
2025-08-02 03:23:04 +0500

    Upd intall img

--- vite.config.ts
+++ vite.config.ts
@@ -43,6 +43,20 @@ export default defineConfig({
             purpose: 'maskable',
           },
         ],
+        screenshots: [
+          {
+            src: "/screenshots/homepage.png",
+            sizes: "640x1136",
+            type: "image/png",
+            form_factor: "narrow"
+          },
+          {
+            src: "/screenshots/dashboard.png",
+            sizes: "1280x720",
+            type: "image/png",
+            form_factor: "wide"
+          }
+        ]
       },
       workbox: {
         navigateFallback: '/offline.html',
